975a778825f7a3c264313e6360aff6cb735b7328
Aleksey Cheusov <vle@gmx.net>
2007-06-18 15:19:13 +0000

    copyright

--- wrappers.c
+++ wrappers.c
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2006-2007, Aleksey Cheusov <vle@gmx.net>
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation.  I make no representations
+ * about the suitability of this software for any purpose.  It is
+ * provided "as is" without express or implied warranty.
+ *
+ */
+
 #include <fcntl.h>
 #include <errno.h>
 #include <stdio.h>
