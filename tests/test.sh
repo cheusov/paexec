8ed03dbaeceeb58a4f4c250d0679e5afe83731d5
Aleksey Cheusov <vle@gmx.net>
2008-01-10 22:45:44 +0000

    clean-ups for file renamings

--- tests/test.sh
+++ tests/test.sh
@@ -29,17 +29,17 @@ resort (){
 }
 
 printf 'a\nbb\nccc\ndddd\neeeee\nffffff\n' |
-runtest -l -t ./paexec_notransport -c ../examples/toupper_task \
+runtest -l -t ./paexec_notransport -c ../examples/toupper/toupper_cmd \
     -n '1 2 3 4 5 6 7 8 9' | resort
 
 printf 'a\nbb\nccc\ndddd\neeeee\nffffff\n' |
-runtest -l -p -t ./paexec_notransport -c ../examples/toupper_task -n '+2' |
+runtest -l -p -t ./paexec_notransport -c ../examples/toupper/toupper_cmd -n '+2' |
 resort | gawk '$1 ~ /^[0-9]/ {$2 = "pid"; print; next} {print}'
 
 printf 'a\nbb\nccc\ndddd\neeeee\nffffff\n' |
-runtest -l -c ../examples/all_substr_task \
+runtest -l -c ../examples/all_substr/all_substr_cmd \
     -n '1 2 3 4 5 6 7 8 9' | resort
 
 printf 'a\nbb\nccc\ndddd\neeeee\nffffff\n' |
-runtest -l -c ../examples/all_substr_task -n '+9' |
+runtest -l -c ../examples/all_substr/all_substr_cmd -n '+9' |
 resort
