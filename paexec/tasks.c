eabf8cd4fd488254ddf7098b96173dde853407b4
Aleksey Cheusov <vle@gmx.net>
2014-06-07 03:20:04 +0300

    Restore WARNERR=yes (tested on gcc,icc,sunstudio)

--- paexec/tasks.c
+++ paexec/tasks.c
@@ -81,8 +81,9 @@ static int tasks_cmp (task_t *a, task_t *b)
 }
 
 static RB_HEAD (tasks_entries, task_struct) tasks = RB_INITIALIZER(&tasks);
-RB_PROTOTYPE (tasks_entries, task_struct, linkage, tasks_cmp);
-RB_GENERATE (tasks_entries, task_struct, linkage, tasks_cmp);
+
+RB_PROTOTYPE (tasks_entries, task_struct, linkage, tasks_cmp)
+RB_GENERATE (tasks_entries, task_struct, linkage, tasks_cmp)
 
 int graph_mode  = 0;
 /* numeric task id to textual representation*/
