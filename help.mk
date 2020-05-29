9cb164683de2d7062ec80f91780e8609d529105f
Aleksey Cheusov <vle@gmx.net>
2020-05-29 17:05:09 +0300

    Add documentation for subproject "tests"

--- help.mk
+++ help.mk
@@ -1,4 +1,5 @@
 HELP_MSG.paexec =	"paexec, paexec_reorder and pareorder executables"
 HELP_MSG.paargs =	"paargs executable"
+HELP_MSG.tests =	'tests for target "test"'
 HELP_MSG.examples =	"samples of use"
 HELP_MSG.doc    =	"LICENSE, README, TODO and NEWS"
