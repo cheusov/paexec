083adeb8222bfd3861efc50b20c9ae2177a25b62
Aleksey Cheusov <vle@gmx.net>
2020-05-31 20:15:49 +0300

    Add USE_COPTS configuring flag

--- use.mk
+++ use.mk
@@ -1,4 +1,7 @@
-USE_VARIABLES += USE_BUFSIZE
+USE_VARIABLES += USE_BUFSIZE USE_COPTS
 USE_BUFSIZE.descr = "Internal buffer size for read(2) and write(2)"
-USE_BUFSIZE.0 = "unset:  defaults to 4096"
+USE_BUFSIZE.0 = "unset:  4096"
 USE_BUFSIZE.1 = "value:  other value"
+USE_COPTS.descr = "Optimization flags"
+USE_COPTS.0 = "unset:  -O2 -g"
+USE_COPTS.1 = "value:  other value"
