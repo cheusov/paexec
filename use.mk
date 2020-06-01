0b62f97ec478f759f630d287b28b8c4205a16f3e
Aleksey Cheusov <vle@gmx.net>
2020-06-01 17:10:18 +0300

    Introduce USE_TEST_BUFSIZES

--- use.mk
+++ use.mk
@@ -1,7 +1,10 @@
-USE_VARIABLES += USE_BUFSIZE USE_COPTS
+USE_VARIABLES += USE_BUFSIZE USE_COPTS USE_TEST_BUFSIZES
 USE_BUFSIZE.descr = "Internal buffer size for read(2) and write(2)"
 USE_BUFSIZE.0 = "unset:  4096"
 USE_BUFSIZE.1 = "value:  other value"
 USE_COPTS.descr = "Optimization flags"
 USE_COPTS.0 = "unset:  -O2 -g"
 USE_COPTS.1 = "value:  other value"
+USE_TEST_BUFSIZES.descr = "Initial buffer sizes for testing"
+USE_TEST_BUFSIZES.0 = "unset:  1 2 3 ... 14 1000 10000"
+USE_TEST_BUFSIZES.1 = "value:  other test sizes"
